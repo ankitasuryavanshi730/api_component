3ab209b20bf4bcd566546ebebe56a0f06d64df43
Ankita Suryavanshi <ankitasuryavanshi730@gmail.com>
2023-03-26 14:11:07 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'api_component';
-}
+export class AppComponent  implements OnInit{
+
+  users:any;
+
+
+  constructor (private http:HttpClient){
+
+
+  }
+
+
+  ngOnInit(): void {
+
+     let observable = this.http.get("https://fakestoreapi.com/products");
+
+      observable.subscribe((result:any)=>{
+
+        this.users = result;
+
+
+
+      })
+
+  }
+
+
+
+
+
+  }
